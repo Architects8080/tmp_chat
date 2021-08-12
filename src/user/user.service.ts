b067b0a901dfd539903835f0be3675d636e31ad6
skdlsco <nicnic4102@gmail.com>
2021-08-12 23:17:27 +0900

    FEAT : #4 add user module with typeorm

---
+++ src/user/user.service.ts
@@ -0,0 +1,38 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>) {}
+
+  async createUser(user: User, nickname: string) {
+    let newUser: User = this.userRepository.create(user);
+    
+    newUser.nickname = nickname;
+    try {
+      return await this.userRepository.insert(newUser);
+    } catch (error) {
+      console.log(error);
+      throw new BadRequestException();
+    }
+  }
+
+  async getUsers() {
+    return this.userRepository.find();
+  }
+
+  async getUserById(id: number) {
+    const user: User = await this.userRepository.findOne(id);
+    if (!user)
+      throw new NotFoundException();
+    return user;
+  }
+
+  async deleteUserById(id: number) {
+    await this.userRepository.delete({id: id});
+  }
+} 
