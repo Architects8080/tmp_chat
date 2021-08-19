9c3b3217d09a441469ea05218d29ce2f1625ed6b
Moongss <mininori@naver.com>
2021-08-20 00:51:31 +0900

    FEAT : #4 auth page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Login from './auth/login';
+import Main from './auth/main';
+import Register from './auth/register';
 
 function App() {
   return (
-    <div className="App">
-      main
-    </div>
+	<div>
+		<Route exact path="/" component={Login}/>
+		<Route path="/main" component={Main}/>
+		<Route path="/register" component={Register}/>
+	</div>
   );
 }
 
---
+++ src/auth/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+	return (
+		<div>
+			it is main view!
+		</div>
+	);
+}
+
+export default Main;
