e8852d8b25c881207f7682bd713898228f9ed3c6
Moongss <mininori@naver.com>
2021-10-02 18:11:23 +0900

    FIX : overlay position

--- src/components/notification/dropdown.tsx
+++ src/components/notification/dropdown.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import NotificationItem from './Item/item';
-import './dropdown.scss';
+import React from "react";
+import NotificationItem from "../dropdown/itemTemplate/notification/item";
+import "./dropdown.scss";
 
 //List를 Main에서 불러오면, 리스트 유무에 따른 icon img 변경
 //Button을 누르면 Dropdown open.
@@ -10,16 +10,41 @@ type DropdownProps = {
 };
 
 function NotificationOverlay(prop: DropdownProps) {
-
   return (
     <div className="notification-wrap">
-      <div className={`dropdown ${prop.isActive ? 'active' : 'inactive'}`}>
-          <NotificationItem/>
-          <NotificationItem/>
-          <NotificationItem/>
-          <NotificationItem/>
+      <div className={`dropdown ${prop.isActive ? "active" : "inactive"}`}>
+        <NotificationItem
+          title="test"
+          description="verylongdescriptionverylongdescriptionverylongdescriptionverylongdescription"
+          acceptCallback={() => {}}
+          rejectCallback={() => {}}
+        />
+        <NotificationItem
+          title="test"
+          description="description"
+          acceptCallback={() => {}}
+          rejectCallback={() => {}}
+        />
+        <NotificationItem
+          title="test"
+          description="description"
+          acceptCallback={() => {}}
+          rejectCallback={() => {}}
+        />
+        <NotificationItem
+          title="test"
+          description="description"
+          acceptCallback={() => {}}
+          rejectCallback={() => {}}
+        />
+        <NotificationItem
+          title="test"
+          description="description"
+          acceptCallback={() => {}}
+          rejectCallback={() => {}}
+        />
       </div>
     </div>
-  )
+  );
 }
 export default NotificationOverlay;
--- src/components/profile/dropdown.tsx
+++ src/components/profile/dropdown.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import './dropdown.scss';
-import ProfileMenuItem from './Item/item';
+import React from "react";
+import HeaderDropdownList from "../dropdown/dropdownList/header";
+import "./dropdown.scss";
 
 type DropdownProps = {
   isActive: boolean;
 };
 
 function ProfileMenu(prop: DropdownProps) {
-
   return (
     <div className="profile-menu-wrap">
-      <div className={`dropdown ${prop.isActive ? 'active' : 'inactive'}`}>
-        <ProfileMenuItem title="프로필 보기" onClick={()=>{window.location.href = "http://localhost:3000/profile"}}/>
-        <ProfileMenuItem title="환경 설정" onClick={()=>{window.location.href = "http://localhost:3000/setting"}}/>
+      <div className={`dropdown ${prop.isActive ? "active" : "inactive"}`}>
+        <HeaderDropdownList />
       </div>
     </div>
   );
