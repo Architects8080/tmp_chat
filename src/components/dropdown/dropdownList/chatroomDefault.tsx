78e018121025d8d5cfc3678e2c146b91d0854d97
Moongss <mininori@naver.com>
2021-10-06 02:01:10 +0900

    FEAT : #18 setting dropdownMenuList according to sidebar type, permission

--- src/components/dropdown/dropdownList/chatroomDefault.tsx
+++ src/components/dropdown/dropdownList/chatroomDefault.tsx
@@ -1,20 +1,37 @@
+import React from "react";
 import AddFriendItem from "../dropdownItem/chatroom/addFriend";
 import InviteGameItem from "../dropdownItem/inviteGame";
 import ObserveGameItem from "../dropdownItem/observeGame";
 import ViewProfileItem from "../dropdownItem/viewProfile";
 import "./dropdownList.scss";
+import { dropdownListProps } from "./dropdownListType";
 
-function ChatroomDefaultDropdownList() {
+function ChatroomDefaultDropdownList(prop: dropdownListProps) {
+  const info = prop.dropdownListInfo;
+  const position = { top: prop.anchorPoint.y, left: prop.anchorPoint.x };
+  {
+    console.log("chatroom default", prop);
+  }
 
   return (
-    <>
-        <div className="dropdown-list-wrap">
-          <ViewProfileItem />
-          <AddFriendItem />
-          <InviteGameItem />
-          <ObserveGameItem />
-        </div>
-    </>
+    <div className="dropdown-list-wrap" style={position}>
+      <ViewProfileItem targetId={info.targetId} />
+      {info.targetId === info.userId ? (
+        ""
+      ) : (
+        <React.Fragment>
+          {!info.isFriend ? <AddFriendItem targetId={info.targetId} /> : ""}
+          {info.isInGame ? (
+            <ObserveGameItem roomId={info.roomId} />
+          ) : (
+            <InviteGameItem
+              targetId={info.targetId}
+              modalHandler={prop.modalHandler}
+            />
+          )}
+        </React.Fragment>
+      )}
+    </div>
   );
 }
 
--- src/components/dropdown/dropdownList/header.tsx
+++ src/components/dropdown/dropdownList/header.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import LogoutItem from "../dropdownItem/header/logout";
 import SettingItem from "../dropdownItem/header/setting";
 import ViewProfileItem from "../dropdownItem/viewProfile";
 import "./dropdownList.scss";
 
 function HeaderDropdownList() {
+  var userId = 0;
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/user/me", { withCredentials: true })
+      .then((response) => {
+        userId = response.data.id;
+      });
+  }, []);
+
   return (
     <div className="dropdown-list-wrap dropdown-header">
-      <ViewProfileItem/>
-      <SettingItem/>
-      <LogoutItem/>
+      <ViewProfileItem targetId={userId} />
+      <SettingItem />
+      <LogoutItem />
     </div>
   );
 }
