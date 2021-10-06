139c0e24021a90e0cec2911493e91a86bfd3bb4b
hysimok <jeonghy95@gmail.com>
2021-10-06 22:23:50 +0900

    resolve conflicts

--- src/views/main/main.tsx
+++ src/views/main/main.tsx
@@ -4,18 +4,49 @@ import EmptyPageInfo from "../../components/emptyPage/empty";
 import Header from "../../components/header/header";
 import ChatroomCreateModal from "../../components/modal/chatroom/create/chatroomCreateModal";
 import SideBar from "../../components/sideBar/sideBar";
-import ChatroomItem from "./chatroomItem/item";
+import ChatroomItem, { chatroomItemProps } from "./chatroomItem/item";
 import "./main.scss";
 import { sidebarProperty } from "../../components/sideBar/sideBarType";
 import ModalHandler from "../../components/modal/modalhandler";
+import ChatroomDefaultDropdownList from "../../components/dropdown/dropdownList/chatroomDefault";
+import ChatroomAdminDropdownList from "../../components/dropdown/dropdownList/chatroomAdmin";
+import ChatroomOwnerDropdownList from "../../components/dropdown/dropdownList/chatroomOwner";
+import FriendDropdownList from "../../components/dropdown/dropdownList/friend";
+import HeaderDropdownList from "../../components/dropdown/dropdownList/header";
+import OTPModal from "../../components/modal/otp/otpModal";
+import axios from "axios";
+import { useEffect } from "react";
 
 enum ChatroomCategory {
   AllChatroomList,
   JoinedChatroomList,
 }
 
-function Main() {
+const Main = () => {
   const modalHandler = ModalHandler();
+  const [isModalOpen, setIsModalOpen] = useState({
+    addFriend: false,
+    chatroomInvite: false,
+    chatroomCreate: false,
+    chatroomSetting: false,
+    enterPassword: false,
+    otp: false,
+  });
+
+  const handleModalOpen = (key: string) => {
+    setIsModalOpen({
+      ...isModalOpen,
+      [key]: true,
+    });
+  };
+
+  const handleModalClose = (key: string) => {
+    setIsModalOpen({
+      ...isModalOpen,
+      [key]: false,
+    });
+  };
+
   const [chatroomCategory, setChatroomCategory] = useState(
     ChatroomCategory.AllChatroomList
   );
@@ -28,24 +59,27 @@ function Main() {
     };
   };
 
-  var tempAllChatroomList = [
-    {
-      roomId: 42,
-      title: "Libft 평가자 모십니다",
-      memberCount: 42,
-      isProtected: true,
-    },
-  ];
+  const [channels, setChannels] = useState<chatroomItemProps[] | null>(null);
+  const [myChannels, setMyChannels] = useState<chatroomItemProps[] | null>(null);
 
-  var tempJoinedChatroomList = [
-    {
-      roomId: 42,
-      title: "트랜센던스 팀원구함",
-      memberCount: 42,
-      isProtected: false,
-    },
-  ];
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				let response = await axios.get(`http://localhost:5000/channel`, { withCredentials: true });
+				setChannels(response.data);
+        response = await axios.get(`http://localhost:5000/channel/me`, { withCredentials: true });
+        setMyChannels(response.data);
+			}
+			catch (e) { console.log(e); }
+		};
+		fetchData();
+	}, [channels]);
 
+  if (!channels) {
+		return (
+			<div>Loading..</div>
+		)
+	}
   // to test empty info
   // tempAllChatroomList = [];
   // tempJoinedChatroomList = [];
@@ -87,37 +121,28 @@ function Main() {
             </div>
           </div>
 
-          {chatroomCategory === ChatroomCategory.AllChatroomList &&
-          tempAllChatroomList.length === 0 ? (
-            <EmptyPageInfo
-              description={`공개 채팅방이 존재하지 않습니다.\n'채팅방 만들기' 버튼으로 채팅방을 생성해보세요!`}
-            />
-          ) : chatroomCategory === ChatroomCategory.JoinedChatroomList &&
-            tempJoinedChatroomList.length === 0 ? (
-            <EmptyPageInfo
-              description={`현재 참여중인 채팅방이 없습니다.\n전체 채팅방 목록에서 참가해보세요!`}
-            />
-          ) : (
-            <div className="chatroom-list">
-              {chatroomCategory === ChatroomCategory.AllChatroomList
-                ? tempAllChatroomList.map((item) => (
-                    <ChatroomItem
-                      roomId={item.roomId}
-                      title={item.title}
-                      memberCount={item.memberCount}
-                      isProtected={item.isProtected}
-                    />
-                  ))
-                : tempJoinedChatroomList.map((item) => (
-                    <ChatroomItem
-                      roomId={item.roomId}
-                      title={item.title}
-                      memberCount={item.memberCount}
-                      isProtected={item.isProtected}
-                    />
-                  ))}
-            </div>
-          )}
+          {(chatroomCategory === ChatroomCategory.AllChatroomList && !channels) 
+            ? <EmptyPageInfo description={`공개 채팅방이 존재하지 않습니다.\n'채팅방 만들기' 버튼으로 채팅방을 생성해보세요!`}/>
+            : (chatroomCategory === ChatroomCategory.JoinedChatroomList && !myChannels)
+              ? <EmptyPageInfo description={`현재 참여중인 채팅방이 없습니다.\n전체 채팅방 목록에서 참가해보세요!`}/> 
+              : <div className="chatroom-list">
+                  {chatroomCategory === ChatroomCategory.AllChatroomList 
+                    ? channels.map(item => (
+                        <ChatroomItem 
+                        channel = {item} 
+                        key = {item.roomId}/>
+                      ))
+                    // : myChannels.map(item => (
+                    //     <ChatroomItem roomId={item.roomId} title={item.title} memberCount={item.memberCount} isProtected={item.isProtected}/>
+                    //   ))
+                       : myChannels ? myChannels.map(item => (
+                          <ChatroomItem 
+                          channel = {item} 
+                          key = {item.roomId}/>
+                        )) : null
+                  }
+                </div>
+          }
         </div>
       </div>
       {modalHandler.isModalOpen.chatroomCreate ? (
