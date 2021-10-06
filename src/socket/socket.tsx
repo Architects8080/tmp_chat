9fd907352d1a3d4b99d4bbeee423cb07b26f461d
hysimok <jeonghy95@gmail.com>
2021-10-06 22:23:50 +0900

    resolve conflicts

--- src/socket/socket.tsx
+++ src/socket/socket.tsx
@@ -3,3 +3,7 @@ import SocketIO from "socket.io-client";
 export const io = SocketIO("http://localhost:4000/game", {
   withCredentials: true,
 });
+
+export const ioChannel = SocketIO("http://localhost:4501/channel", {
+  withCredentials: true,
+})
--- src/views/chatroom/chatroom.tsx
+++ src/views/chatroom/chatroom.tsx
@@ -5,6 +5,8 @@ import SideBar from "../../components/sideBar/sideBar";
 import { sidebarProperty } from "../../components/sideBar/sideBarType";
 import "./chatroom.scss";
 import ChatMessage from "./message/message";
+import { ioChannel } from "../../socket/socket";
+import { useParams } from "react-router-dom";
 
 const AlwaysScrollToBottom = () => {
   const elementRef = useRef<HTMLDivElement | null>(null);
@@ -12,9 +14,52 @@ const AlwaysScrollToBottom = () => {
   return <div ref={elementRef} />;
 };
 
-function Chatroom() {
-  const modalHandler = ModalHandler();
+// 서버로부터 받아서 message state 에 넣을 때 들어가는 형태
+type Message = {
+  id: string;
+  name: string;
+  text: string;
+};
 
+// 서버로부터 받는 메시지 형태
+type Payload = {
+  id: number;
+  name: string;
+  text: string;
+}
+
+const Chatroom = () => {
+  const modalHandler = ModalHandler();
+  const [messages, setMessages] = useState<Message[]>([]);
+  
+  const [text, setText] = useState('');
+  let { id } : any = useParams();
+  useEffect(() => {
+    function receivedMessage(message: Payload) {
+      const newMessage: Message = {
+        id: uuid.v4(),
+        name: message.name,
+        text: message.text,
+      };
+
+      setMessages([...messages, newMessage]);
+    }
+
+    ioChannel.on('msgToClient', (message: Payload) => {
+      receivedMessage(message);
+    });
+  }, [messages, text]);
+
+  const sendMessage = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key !== 'Enter' || text === '')
+			return;
+		const newMessageSend = {
+      roomId: id,
+      text: text,
+		};
+		ioChannel.emit('msgToChannel', newMessageSend);
+		setText('');
+	}
   return (
     <>
       <Header isLoggedIn={true} />
@@ -25,35 +70,19 @@ function Chatroom() {
           modalHandler={modalHandler}
         />
         <div className="chatroom-wrap">
-          <div className="chatroom-message-list-wrap">
-            <div className="chatroom-message-list">
-              {/* map->showing message */}
-              <ChatMessage
-                isSelfMessage={false}
-                nickname="chlee"
-                content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          <div className="chatroom-message-list">
+            {messages.map(message => (
+              <ChatMessage key={message.id} isSelfMessage={true} nickname={message.name} content={message.text}/>
+            ))}
+            <ChatMessage isSelfMessage={false} nickname="chlee" content="test" />
+            <div className="chatroom-user-input">
+              <input 
+                className="input-field"
+                placeholder="내용을 입력하세요"
+                value={text}
+                onChange={e => setText(e.target.value)}
+                onKeyPress={sendMessage}
               />
-              <ChatMessage
-                isSelfMessage={true}
-                nickname="chlee"
-                content="test"
-              />
-              <ChatMessage
-                isSelfMessage={false}
-                nickname="chlee"
-                content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-              />
-              <ChatMessage
-                isSelfMessage={true}
-                nickname="chlee"
-                content="test"
-              />
-              <ChatMessage
-                isSelfMessage={true}
-                nickname="chlee"
-                content="test"
-              />
-              <AlwaysScrollToBottom />
             </div>
           </div>
           <div className="chatroom-user-input">
