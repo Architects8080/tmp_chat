2363ec70d85fadb064ccb6f82cd0564f90451e07
Moongss <mininori@naver.com>
2021-10-20 17:53:27 +0900

    FEAT : #68 register result setting

--- src/views/auth/register/register.tsx
+++ src/views/auth/register/register.tsx
@@ -44,12 +44,16 @@ function Register() {
           setRegisted(true);
         })
         .catch(error => {
+          console.log(`error : `, error.response.data);
           if (error.response.data.statusCode === 403) {
             setForbiddenError(true);
             snackbar.error("잘못된 접근입니다.");
           }
           else if (error.response.data.statusCode === 400) {
-            snackbar.error("닉네임이 중복되었습니다. 다시 시도해주세요.");
+            if (error.response.data.message == "TOO LONG NICKNAME")
+              snackbar.error("닉네임은 최대 10자까지 가능합니다.");
+            else
+              snackbar.error("닉네임이 중복되었습니다. 다시 시도해주세요.");
           }
         });
     } catch (error) {};
